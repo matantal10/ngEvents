5bcf5e7a2c328f101d3acaabab765e73bc60582f
mt <matan.tal1009@gmail.com>
2019-07-27 17:00:20 +0300

    add reactive form

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl} from '@angular/forms';
+import { AuthService } from './auth.service';
+import {Router} from '@angular/router';
 @Component({
   selector: 'profile',
   templateUrl: './profile.component.html',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  formProfile: FormGroup;
+  firstName;
+  lastName
+  constructor(private authService: AuthService, private router:Router) { }
 
   ngOnInit() {
+    this.firstName = new FormControl(this.authService.currentUser.firstName);
+    this.lastName = new FormControl(this.authService.currentUser.lastName);
+    
+    this.formProfile = new FormGroup({
+      firstName: this.firstName,
+      lastName: this.lastName
+    });
+  }
+
+  cancel() {
+    this.router.navigate(['events']);
+  }
+
+  saveProfile(formProfile) {
+    this.authService.updateCurrentUser(formProfile.firstName, formProfile.lastName);
+    this.router.navigate(['events']);
   }
 
 }
