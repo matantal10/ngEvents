61d03c299c4b7cd9b6e0a676d262ca179b1bd8a2
matant1 <matant@mprest.com>
2019-09-25 10:42:53 +0300

    Adding subscription for subscribe and unsubscribe methods

--- src/app/events/events-list/events-list.component.ts
+++ src/app/events/events-list/events-list.component.ts
@@ -1,22 +1,24 @@
 import { Component, OnInit, OnDestroy} from '@angular/core';
 import {EventService} from '../../shared/event.service';
 import { IEvent } from '../../shared/IEvent';
+import {Observable, Subscription} from 'rxjs';
 
 @Component({
   selector: 'events-list',
   templateUrl: './events-list.component.html',
   styleUrls: ['./events-list.component.scss'],
 })
-export class EventsListComponent implements OnInit {
+export class EventsListComponent implements OnInit, OnDestroy {
 
   public data: any;
   public events: IEvent[];
+  private subscription: Subscription = new Subscription();
 
   constructor(private es: EventService) {
   }
 
   ngOnInit() {
-    this.es.getEvents().subscribe(events => {
+    this.subscription = this.es.getEvents().subscribe(events => {
       this.events = events;
     });
   }
@@ -24,5 +26,9 @@ export class EventsListComponent implements OnInit {
   handleEventClicked(data) {
     console.log(data);
     this.data = data;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
