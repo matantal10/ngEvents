ffd9ce2fc01c1f759c0553081219fc51396bc2a3
matant1 <matant@mprest.com>
2019-10-28 12:02:51 +0200

    Implementing dropdown button on navbar

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -1,20 +1,26 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { AuthService } from '../user/auth.service';
 import {EventService} from '../shared/event.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'nav-bar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
 
   public sessionTerm = '';
   public foundSessions: [];
+  public events;
+  public subscription = new Subscription();
 
   constructor(private authService: AuthService, private eventService: EventService) { }
 
   ngOnInit() {
+    this.subscription = this.eventService.getEvents().subscribe(events => {
+      this.events = events;
+    });
   }
 
   searchSessions(seesionTerm: any) {
@@ -22,5 +28,9 @@ export class NavbarComponent implements OnInit {
       this.foundSessions = sessions;
       console.log(this.foundSessions);
     });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
